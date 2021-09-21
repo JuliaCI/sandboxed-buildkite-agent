ad0d464eda59f1d323045fe293073ee0b2020611
Elliot Saba <staticfloat@gmail.com>
2021-09-21 16:53:56 -0400

    Just don't export it

--- hooks/environment.d/101_julia_num_cores.sh
+++ hooks/environment.d/101_julia_num_cores.sh
@@ -4,5 +4,5 @@
 # If you want a different value, I suggest overriding this value within
 # another environment hook in your `environment.local.d` directory,
 # which is appropriately `.gitignore`'d to maintain a local config.
-#export JULIA_NUM_THREADS=$(($(nproc) > 16 ? 16 : $(nproc)))
+JULIA_NUM_THREADS=$(($(nproc) > 16 ? 16 : $(nproc)))
 export JULIA_CPU_THREADS=${JULIA_NUM_THREADS}
