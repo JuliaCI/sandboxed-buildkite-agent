585eed610a1865189220c2c20dbd79bacb7411db
Elliot Saba <staticfloat@gmail.com>
2022-02-02 04:36:59 +0000

    Fix setup happening every boot

--- windows-kvm/winbase/setup_scripts/setup.ps1
+++ windows-kvm/winbase/setup_scripts/setup.ps1
@@ -1,17 +1,19 @@
 # Receive an explicit stage so that we can reboot and resume a new stage
 param([Int32]$stage=0)
+Write-Output "setup.ps1 starting with stage $stage"
 
 # Save all output to packer_setup.log
 Start-Transcript -Append -Path "$env:LOCALAPPDATA\packer_setup.log"
 
 while ($true) {
-    Write-Output "Setup Stage $stage initiating"
     $stage_scripts = Get-ChildItem $PSScriptRoot -Filter ${stage}-*-*.ps1
 
     # If we've run off the end of the scripts, don't continue running
     if( $stage_scripts.count -le 0 ) {
+        Write-Output "Setup complete!"
         break;
     }
+    Write-Output "Setup Stage $stage initiating"
 
     # Run each script
     $stage_scripts | ForEach-Object {
