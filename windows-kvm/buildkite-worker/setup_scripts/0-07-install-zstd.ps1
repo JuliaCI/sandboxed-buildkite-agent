8e278bad6445a783a84f53e1cdcaeeb2ef6422d1
Elliot Saba <staticfloat@gmail.com>
2022-11-09 13:55:48 -0800

    Fix typo in zstd installation script

--- windows-kvm/buildkite-worker/setup_scripts/0-07-install-zstd.ps1
+++ windows-kvm/buildkite-worker/setup_scripts/0-07-install-zstd.ps1
@@ -2,7 +2,7 @@ $version = "1.5.2"
 Write-Output "Installing zstd v$version..."
 $zstdUrl = "https://github.com/facebook/zstd/releases/download/v${version}/zstd-v${version}-win64.zip"
 $zstdZip = Join-Path $env:TEMP 'zstd.zip'
-Invoke-WebRequest -Uri $telegrafUrl -OutFile $telegrafZip -ErrorAction Stop
+Invoke-WebRequest -Uri $zstdUrl -OutFile $zstdZip -ErrorAction Stop
 
 Expand-Archive -Path $zstdZip -DestinationPath "$env:TEMP" -ErrorAction Stop -Force
 Move-Item -Path (Join-Path $env:TEMP "zstd-v${version}-win64/zstd.exe") `
