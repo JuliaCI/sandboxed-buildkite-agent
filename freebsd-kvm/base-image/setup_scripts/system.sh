562521c15465502e8fce159b6ba5c903a4fa55dc
Elliot Saba <staticfloat@gmail.com>
2022-12-04 01:42:56 +0000

    Use `root` for provisioning
    We need to do a lot of `root`-like things, so let's just be `root`.  We
    enable password-based auth so that packer can properly connect.

--- freebsd-kvm/base-image/setup_scripts/system.sh
+++ freebsd-kvm/base-image/setup_scripts/system.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -e
 sysrc clear_tmp_enable=YES
-sed -i '' -e 's/^PermitRootLogin yes/#PermitRootLogin no/' /etc/ssh/sshd_config
-sed -i '' -e 's/^#UsePAM yes/UsePAM no/' /etc/ssh/sshd_config
+#sed -i '' -e 's/^PermitRootLogin yes/#PermitRootLogin no/' /etc/ssh/sshd_config
+#sed -i '' -e 's/^#UsePAM yes/UsePAM no/' /etc/ssh/sshd_config
+sed -i '' -e 's/^#PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
