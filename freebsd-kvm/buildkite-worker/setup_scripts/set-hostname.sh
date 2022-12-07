8fa6768928f414b8e12786f7bbf7f1b058ecdf09
Alex Arslan <ararslan@comcast.net>
2022-12-07 13:58:12 -0800

    Make hostname change persistent

--- freebsd-kvm/buildkite-worker/setup_scripts/set-hostname.sh
+++ freebsd-kvm/buildkite-worker/setup_scripts/set-hostname.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
+
 echo "-> Setting hostname to ${SANITIZED_HOSTNAME}"
+# Change for the current session
 hostname "${SANITIZED_HOSTNAME}"
+# Make the change persistent across reboots by modifying the value in `/etc/rc.conf`
+sysrc hostname="${SANITIZED_HOSTNAME}"
