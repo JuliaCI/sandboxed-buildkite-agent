30d475ded932208c2afcfc1034410fbf2b716a54
Elliot Saba <staticfloat@gmail.com>
2023-01-23 09:13:04 -0800

    Sf/windows debugging (#43)
    * Windows debugging
    
    * Add `publish` step so I can rebuild things without affecting running agents
    
    * Fix debugging hook
    
    * Silence `groups` error output
    
    * Prefer shell expansion instead of backticks
    
    * Limit windows debugging output a bit

---
+++ hooks/environment.d/500_windows_debugging.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+LOG_FILES=(
+    # This log file should get cleared out every boot
+    "${HOME}/startup.log"
+    # This log file should persist as long as the cache drive does
+    "$(cygpath "${BUILDKITE_PLUGIN_JULIA_CACHE_DIR}")/startup.log"
+)
+
+function log() {
+    # Read the message in from stdin
+    MSG="$(</dev/stdin)"
+
+    # Write it out to log files
+    for LOG_FILE in "${LOG_FILES[@]}"; do
+        LOG_FILE_DIR="$(dirname "${LOG_FILE}")"
+        mkdir -p "${LOG_FILE_DIR}"
+        cat >>"${LOG_FILE}" <<<"${MSG}"
+    done
+}
+
+function debug_startup() {
+    # First, say who and when we are
+    hostname | log
+    date | log
+
+    # Next, show some buildkite data
+    echo "buildkite processes:" | log
+    ps --windows | grep buildkite | log
+    echo "buildkite variables:" | log
+    set | grep -i BUILDKITE_.*_ID= | log
+    set | grep -i BUILDKITE_AGENT_PID | log
+    set | grep -i BUILDKITE_BUILD_NUMBER | log
+
+
+    # At the end, spit the log out:
+    for LOG_FILE in "${LOG_FILES[@]}"; do
+        echo "--- Startup Debugging (${LOG_FILE})"
+        cat "${LOG_FILE}"
+    done
+    echo "--- rest of startup"
+}
+
+
+if [[ "$(uname 2>/dev/null)" == MINGW* ]]; then
+    debug_startup
+fi
