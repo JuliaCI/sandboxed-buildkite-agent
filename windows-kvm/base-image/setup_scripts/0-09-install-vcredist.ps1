90331ddfdee1a63805a8fbc1ddea883c6936dd89
Fredrik Ekre <ekrefredrik@gmail.com>
2026-02-11 19:14:03 +0100

    Install NuGet before installing Visual C++ redistributables
    Fixes #115

--- windows-kvm/base-image/setup_scripts/0-09-install-vcredist.ps1
+++ windows-kvm/base-image/setup_scripts/0-09-install-vcredist.ps1
@@ -1,2 +1,7 @@
+# The install.ps1 script require NuGet and if it can't be found it will prompt
+# the user so we install explicitly so that the build can proceed without user
+# input.
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+
 # Install all the Visual C++ redistributables
 iex ((New-Object System.Net.WebClient).DownloadString('https://vcredist.com/install.ps1'))
