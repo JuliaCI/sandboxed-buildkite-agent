af88ca1467b059d96e24e4b92458ed1de2f06e74
Tim Besard <tim.besard@gmail.com>
2026-07-01 17:35:17 +0200

    FreeBSD: fix SSH key provision.

--- platforms/freebsd-kvm/base-image/setup_scripts/secrets.sh
+++ platforms/freebsd-kvm/base-image/setup_scripts/secrets.sh
@@ -2,7 +2,7 @@
 set -e
 SSH_DIR="/usr/home/${USER}/.ssh"
 mkdir -p "${SSH_DIR}"
-cat /tmp/ssh_keys/* > "${SSH_DIR}/authorized_keys"
+find /tmp/ssh_keys -type f -exec cat {} + > "${SSH_DIR}/authorized_keys"
 chmod -R 700 "${SSH_DIR}"
 chmod 600 "${SSH_DIR}/authorized_keys"
 chown -R ${USER}:${USER} "${SSH_DIR}"
